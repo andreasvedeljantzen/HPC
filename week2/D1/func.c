3314a58aa7f5cf8d39200fda065c37a4593f06f0
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-08 15:28:11 +0100

    pi cal10

--- week2/D1/func.c
+++ week2/D1/func.c
@@ -1,9 +1,9 @@
 /*------PI----*/
 double pi_cal(int I) {
-	double PI[I];
-	double RealPI;
+	double PI[I] ;
+	double RealPI = 0.0;
 	for (int i = 1; i <= I; i++) { 
-		PI[i-1] = (4/((1+(((i-0.5)/I)*((i-0.5)/I)))));
+		PI[i-1] = 4/(1+(i-0.5)/I*(i-0.5)/I);
     }
     for (int i = 0; i < I; i++) { 
 		RealPI += PI[i];
