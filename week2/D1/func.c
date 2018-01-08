38729d7f179c7bfcb057998f117045051ebd72f5
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-08 14:53:12 +0100

    pi cal5

--- week2/D1/func.c
+++ week2/D1/func.c
@@ -1,7 +1,7 @@
 /*------PI----*/
 void pi_cal(int I) {
 	double temp;
-		for (int i = 0; i < I; n++) { 
-		PI[i] += (4/((1+pow((i-0.5)/I),2)))
+		for (int i = 0; i < I; i++) { 
+		PI += (4/((1+pow((i-0.5)/I),2)))
     }
 }
