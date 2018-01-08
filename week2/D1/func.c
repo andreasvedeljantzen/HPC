5ce553676adefb8be0955f49cb87c142d3cc5b1b
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-08 14:35:27 +0100

    pi cal

--- week2/D1/func.c
+++ week2/D1/func.c
@@ -1,9 +1,7 @@
 /*------PI----*/
-void matmult_nat(int M, int N, int K, double **A, double **B, double **C) {
-	for (int l = 0; l < M*N; l++) {
-	temp = 0;
-	}
-		for (int n = 0; n < N; n++) { 
-		temp = temp + (4/((1+pow((i-0.5)/n),2)))
+void pi_cal(int I) {
+	double temp;
+		for (int i = 0; i < I; n++) { 
+		PI += (4/((1+pow((i-0.5)/I),2)))
     }
 }
---
+++ week2/D1/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "datatools.h"		/* helper functions	        */
+#include "matadd.h"		/* my matrix add fucntion	*/
+
+#define NREPEAT 100		/* repeat count for the experiment loop */
+
+int
+main(int argc, char *argv[]) {
+
+    int    I;
+    I = 100;
+	    
+	printf(pi_cal(N));
+
+    return EXIT_SUCCESS;
+}
