f15c56bfbeb18ec55241339a799fd67597ac13f4
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-08 16:20:52 +0100

    paralelliseret

--- week2/D1/func.c
+++ week2/D1/func.c
@@ -1,11 +1,17 @@
 /*------PI----*/
-
+#include <omp.h>
 double pi_cal(int I) {
 	double PI[I] ;
 	double RealPI = 0.0;
-	for (int i = 1; i <= I; i++) { 
+	int i;
+
+	#pragma omp parallel shared(I,PI) private(i) 
+	{
+	#pragma omp for
+	for (i = 1; i <= I; i++) { 
 		PI[i-1] = 4.0/(1.0+((i-0.5)/I)*((i-0.5)/I));
-    }
+    	}
+	} /* end of parallel region */
     for (int i = 0; i < I; i++) { 
 		RealPI += PI[i];
     }
