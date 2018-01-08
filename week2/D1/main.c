240eb04a38bdfedc20e543faa0cc1e7b5eb7a279
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-08 15:59:16 +0100

    anja pi cal3

--- week2/D1/main.c
+++ week2/D1/main.c
@@ -8,8 +8,10 @@ main(int argc, char *argv[]) {
 
     int    I;
     I = 1000000;
+
+    double PI = pi_cal(I);
 	    
-	printf("Pi = %g\n", pi_cal(I));
+	printf("Pi = %g\n", PI);
 
     return EXIT_SUCCESS;
 }
