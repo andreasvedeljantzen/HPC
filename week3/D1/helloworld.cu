5b3288a13430f7e0383cf596708252531e6e8390
Jantzen <andreasvedeljantzen@gmail.com>
2018-01-15 15:08:27 +0100

    anders hjælper

--- week3/D1/helloworld.cu
+++ week3/D1/helloworld.cu
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <helper_cuda.h>
-__global__ void kernel();
 
+
+__global__ void kernel() {
+		int i = blockIdx.x * blockDim.x + threadIdx.x;
+		printf("Hello world! Im thread %i out of %i . My Global thread id is %i out of %i \n", threadIdx.x, blockIdx.x, i, gridDim.x*blockDim.x );
+	};
 
 int main(int argc, char **argv)
 {
+	const int device = 0;
+	cudaSetDevice(device); 
+
 	// Kernel lauch
 	kernel<<<4, 64>>>();
 
@@ -12,7 +19,3 @@ int main(int argc, char **argv)
 
 };
 
-__global__ void kernel() {
-		int i = blockIdx.x * blockDim.x + threadIdx.x;
-		printf("Hello world! Im thread %i out of %i . My Global thread id is %i out of %i \n", threadIdx.x, blockIdx.x, i, gridDim.x*blockDim.x );
-	};
